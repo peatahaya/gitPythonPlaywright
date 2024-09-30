55708c2c6fd1ae53cad7acd5b28fd38b1a520d97
Piotr Bielicki <piotrbi3licki@gmail.com>
2024-09-30 14:12:54 +0200

    dividing test_test.py into 2 separetes files: test_remove_item.py & test_end_to_end_flow.py and updating them

--- POM/tests/data.py
+++ POM/tests/data.py
@@ -6,3 +6,18 @@ password = 'secret_sauce'
 
 # Invalid user
 usernameInvalid = 'user'
+
+# Urls
+inventory_url = 'https://www.saucedemo.com/inventory.html'
+item_url = 'https://www.saucedemo.com/inventory-item.html?id=0'
+cart_url = 'https://www.saucedemo.com/cart.html'
+checkout_step_one_url = 'https://www.saucedemo.com/checkout-step-one.html'
+checkout_step_two_url = 'https://www.saucedemo.com/checkout-step-two.html'
+checkout_complete_url = 'https://www.saucedemo.com/checkout-complete.html'
+
+# Item
+item_name = "Sauce Labs Bike Light"
+item_desc = ("A red light isn't the desired state in "
+             "testing but it sure helps when riding your bike at night. "
+             "Water-resistant with 3 lighting modes, 1 AAA battery included.")
+item_price = "9.99"
